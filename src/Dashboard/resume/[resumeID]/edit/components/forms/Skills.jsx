0a68320ba2ed944d3ddd76813006e4b0171f0802
vostok2003 <harshitgumber420@gmail.com>
2025-11-21 19:34:10 +0530

    fix: correct GlobalApi import case sensitivity

--- src/Dashboard/resume/[resumeID]/edit/components/forms/Skills.jsx
+++ src/Dashboard/resume/[resumeID]/edit/components/forms/Skills.jsx
@@ -64,7 +64,7 @@ function Skills() {
   return (
     <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
       <h2 className='font-bold text-lg'>Skills</h2>
-      <p>Add your top Professional key skills</p>
+      <p>Add your top professional key skills</p>
 
       <div>
         {
